2ebd5685a18f14515897f8de4b29dd54ee1d96e5
gelo4891 <jgelo4891@gmail.com>
2023-03-02 16:59:52 +0200

    Ver 3.2 create button work

---
+++ WC_1_create_but/WC_1_index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Кнопки</title>
+	<link rel="stylesheet" href="WC_1_css/WC_1_first.css">
+</head>
+<body class="WTK_index_body">
+	<!-- Місце для відображення кнопок -->
+	<div class="WTK_index_div"><a class="buttons-container"></a></div>
+
+	<!-- Підключення JavaScript-файлів -->
+	<script src="WC_1_js/WC_1_class_Create.js"></script>
+	<script src="WC_1_config/WC_1_button.json"></script>
+	<script>
+		const buttonCreator = new ButtonCreator("WC_1_config/WC_1_button.json", "buttons-container");
+		buttonCreator.createButtons();
+	</script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,19 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Кнопки</title>
-	<link rel="stylesheet" href="WTC_css/WTC_first.css">
-</head>
-<body class="WTK_index_body">
-	<!-- Місце для відображення кнопок -->
-	<div class="WTK_index_div"><a class="buttons-container"></a></div>
-
-	<!-- Підключення JavaScript-файлів -->
-	<script src="WTC_js/class_Create.js"></script>
-	<script src="WTC_config/WTC_button.json"></script>
-	<script>
-		const buttonCreator = new ButtonCreator("WTC_config/WTC_button.json", "buttons-container");
-		buttonCreator.createButtons();
-	</script>
-</body>
-</html>
+<?php
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTP_HOST'];
+	header('Location: '.$uri.'/WC_1_create_but/WC_1_index.php');
+	exit;
+?>
