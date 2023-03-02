464ec0b155379622e1f71c6369037b758010f060
gelo4891 <jgelo4891@gmail.com>
2023-03-02 13:37:02 +0200

    Ver 3.1 create button work

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<title>Кнопки</title>
 	<link rel="stylesheet" href="WTC_css/WTC_first.css">
 </head>
-<body>
+<body class="WTK_index_body">
 	<!-- Місце для відображення кнопок -->
 	<div class="WTK_index_div"><a class="buttons-container"></a></div>
 
